c220f0997de70ac6caf197c1afac5430585f0262
JuanCdeleon <143768170+JuanCdeleon@users.noreply.github.com>
2025-01-30 21:07:17 +0000

    1st commit

---
+++ src/MixMatch.java
@@ -0,0 +1,22 @@
+public class MixMatch{
+    public static void main(String[] args){
+        int a = 40;
+        System.out.println("The integer stores numbers like: " + a);
+        String b = "This";
+        System.out.println("The string holds word or phrases like: " + b);
+        char c = '+';
+        System.out.println("The character stores letters or signs like: " + c);
+        short d = -32766;
+        System.out.println("The short numner stores whole numbers from -32000 to 32000 like: " + d);
+        long e = 223385;
+        System.out.println("Long variables stores, also, whole numbers from big ranges between -9 million to 9 miillion and more like: " + e);
+        double f = 3.423;
+        System.out.println("Double numbers stores 15 to 16 decimal digits: " + f);
+        float g = 19.49f;
+        System.out.println("This float numbers stores 6 to 7 decimal digits: " + g);
+        boolean h = true;
+        System.out.println("This boolean stores true or false values: " + h);
+
+        System.out.println(b + " number " + a + " is " + h + " and it is not " + d);
+    }
+}
